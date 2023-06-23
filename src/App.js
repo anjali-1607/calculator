65dd5e73d7972a9eab3de5ca797f17dfd6134a4f
anjali-1607 <anjalirajpoot1609@gmail.com>
2023-06-23 22:28:23 +0530

    designing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const one = () => {};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="grand">
+        <div className="grand_parent">
+          <div className="parent">
+            <input type="text "></input>
+            <button className="child">Cl</button>
+            <button className="child">DEL</button>
+            <button className="child">%</button>
+            <button className="child">/</button>
+            <button className="child">7</button>
+            <button className="child">8</button>
+            <button className="child">9</button>
+            <button className="child">*</button>
+            <button className="child">4</button>
+            <button className="child">5</button>
+            <button className="child">6</button>
+            <button className="child">-</button>
+            <button className="child" onClick={one}>
+              1
+            </button>
+            <button className="child">2</button>
+            <button className="child">3</button>
+            <button className="child">+</button>
+            <button className="child">0</button>
+            <button className="child">.</button>
+            <button className="equal">=</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
